f28fdb6a91240d1bb7cab4cdd136e657738dccd1
mariem86 <you@example.com>
2020-09-14 11:33:39 +0200

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-
+var express = require('express');
+var app = express();
 const axios = require('axios')
-
+const port = 3000
 const requestOne = axios.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid=b77514d18a7138aafc9ed1bbdca4f85b");
 const requestTwo = axios.get("http://api.openweathermap.org/data/2.5/group?id=524901,703448,2643743&units=metric&appid=b77514d18a7138aafc9ed1bbdca4f85b");
 const requestThree = axios.get("http://api.openweathermap.org/data/2.5/find?lat=55.5&lon=37.5&cnt=10&appid=b77514d18a7138aafc9ed1bbdca4f85b");
@@ -18,3 +19,4 @@ axios
   .catch(errors => {
     console.error(errors);
   })
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
